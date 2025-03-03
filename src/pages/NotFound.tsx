e1f29a9662523ff56070de50542918b76ffa82e7
Xavier K. <59243836+xk2800@users.noreply.github.com>
2025-03-03 21:36:28 +0800

    add 404 page

--- src/pages/NotFound.tsx
+++ src/pages/NotFound.tsx
@@ -1,7 +1,17 @@
+import { Button } from "@/components/ui/button";
+import { Link } from "react-router-dom";
+
 const NotFound = () => {
   return (
-    <div>
-      404 lor
+    <div className="flex flex-col items-center justify-center h-screen text-center px-4 overflow-hidden">
+      <p className="mb-4 text-2xl">Looks like we hit a stang, we were not able to find the page you were looking for.</p>
+      <p className="mb-2 text-xl">Let's try that again.</p>
+      <Button
+        asChild
+        className="border-1 border-[#49f627] hover:bg-[#49f627] hover:border-black hover:text-black transition-all duration-300 ease-in-out cursor-pointer"
+      >
+        <Link to={'/'}>Homepage</Link>
+      </Button>
     </div>
   );
 }
